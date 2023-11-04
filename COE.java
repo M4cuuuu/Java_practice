1465af81e154ee2db9f57d91cdb8bd9249fadb08
M4cuuuu <tinduganmarkjayt@gmail.com>
2023-11-04 13:01:15 +0800

    update

--- COE.java
+++ COE.java
@@ -1,14 +1,34 @@
 import java.util.Scanner;
 
+//class declaration
 class student {
-  public String firstName;
- public String lastName;
- public int age;
- public String program;
- public char block;
- public int yearLvl;
+
+  String firstName;
+  String lastName;
+  int age;
+  String program;
+  char block;
+  int yearLvl;
+
+  //student object
+ public student( String firstName, String lastName, int age, String program, char block, int yearLvl) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.program = program;
+        this.block = block;
+        this.yearLvl = yearLvl;
+}
+
  
 }
+
+//list of courses for different block
+class courses {
+
+  
+}
+
 public class COE {
  
 
@@ -16,32 +36,11 @@ public class COE {
      public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
-       
-
         String school = "Catanduanes State University";
         String schoolAdd = "Virac, Catanduanes";
         String cert = "CERTIFICATE OF ENROLMENT";
         
-        // System.out.println("First name:");
-        // firstName = scanner.nextLine();
-
-        // System.out.println("Last name:");
-        // LastName = scanner.nextLine();
-
-        // System.out.println("Age:");
-        // age = scanner.nextInt();
-        // scanner.nextLine();
-
-        // System.out.println("Block:");
-        // block = scanner.nextLine();
-
-        // System.out.println("Year level:");
-        // yearLvl = scanner.nextInt();
-        // scanner.nextLine();
-
-        // System.out.println("Program: ");
-        // program = scanner.nextLine();
+      
 
 
         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
@@ -50,8 +49,7 @@ public class COE {
         System.out.println("\t\t\t\t\t\t\t\t\t\t      " + schoolAdd);
         System.out.println("\t\t\t\t\t\t\t\t\t\t  "+ cert);
         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-        System.out.printf("Student Name: %s %s \t\t\t\t Program: %s", firstName,lastName,program);
-        
+      
     }
 
 
