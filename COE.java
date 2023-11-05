e6349a3a6f6334ec291f66b7680c2b769aa5031b
M4cuuuu <tinduganmarkjayt@gmail.com>
2023-11-05 11:33:14 +0800

    commit

--- COE.java
+++ COE.java
@@ -39,11 +39,48 @@ class courses {
       course[0].courseName = "Computer Programming";
       course[0].courseCode = "CC103";
       course[0].sched = "T / F";
+      course[1].courseName = "Computer Programming";
+      course[1].courseCode = "CC103";
+      course[1].sched = "T / F";
+      course[2].courseName = "Computer Programming";
+      course[2].courseCode = "CC103";
+      course[2].sched = "T / F";
+      course[3].courseName = "Computer Programming";
+      course[3].courseCode = "CC103";
+      course[3].sched = "T / F";
+      course[4].courseName = "Computer Programming";
+      course[4].courseCode = "CC103";
+      course[4].sched = "T / F";
+      course[5].courseName = "Computer Programming";
+      course[5].courseCode = "CC103";
+      course[5].sched = "T / F";
+      course[6].courseName = "Computer Programming";
+      course[6].courseCode = "CC103";
+      course[6].sched = "T / F";
     }
 
   }
-  
 
+
+public void header() {
+  
+        String school = "Catanduanes State University";
+        String schoolAdd = "Virac, Catanduanes";
+        String cert = "CERTIFICATE OF ENROLMENT";
+
+
+         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+        System.out.println("\t\t\t\t\t\t\t\t\t\t"+ school);
+        System.out.println("\t\t\t\t\t\t\t\t\t\t      " + schoolAdd);
+        System.out.println("\t\t\t\t\t\t\t\t\t\t  "+ cert);
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+}
+  
+public void b() {
+
+
+}
 
 public class COE {
  
@@ -52,19 +89,12 @@ public class COE {
      public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String school = "Catanduanes State University";
-        String schoolAdd = "Virac, Catanduanes";
-        String cert = "CERTIFICATE OF ENROLMENT";
+        
         
       
 
 
-        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-
-        System.out.println("\t\t\t\t\t\t\t\t\t\t"+ school);
-        System.out.println("\t\t\t\t\t\t\t\t\t\t      " + schoolAdd);
-        System.out.println("\t\t\t\t\t\t\t\t\t\t  "+ cert);
-        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+       
       
     }
 
