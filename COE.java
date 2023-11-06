b025da5d42bc1af7fc7bf5f11a547adc6956fc4c
M4cuuuu <tinduganmarkjayt@gmail.com>
2023-11-06 09:27:02 +0800

    new update

--- COE.java
+++ COE.java
@@ -26,8 +26,11 @@ void studentD () {
 
   System.out.printf("Student name: %s %s \n", firstName,lastName);
   System.out.printf("age: %f",age);
+  System.out.println();
 
 }//method for student details this include the information regarding the student object
+
+
 
  
 }
@@ -35,36 +38,50 @@ void studentD () {
 //list of courses 
 class courses {
 
+  //course details
   String courseName;
   String courseCode;
+  int units;
  
 
     //course object
-    public courses(String courseName, String courseCode, String sched) {
+    public courses(String courseName, String courseCode, int units) {
       
-      //array object initialization
-      courses [] course = new courses[7];
+      //arrays of object course initialization
+      courses [] course = new courses[8];
 
+      //course object and it's values
       course[0].courseName = "Computer Programming 2";
       course[0].courseCode = "CC103";
+      course[0].units = 4;
       
-      course[1].courseName = "English";
-      course[1].courseCode = "ASDF123";
+      course[1].courseName = "Fundamentals of Information System";
+      course[1].courseCode = "IS211";
+      course[1].units = 3;
       
-      course[2].courseName = "Math";
-      course[2].courseCode = "ADW12";
+      course[2].courseName = "Organization and Management Concept";
+      course[2].courseCode = "IS212";
+      course[2].units = 3;
       
-      course[3].courseName = "Computer Programming";
-      course[3].courseCode = "CC103";
+      course[3].courseName = "Art Appreciation";
+      course[3].courseCode = "GEC6";
+      course[3].units = 2;
       
-      course[4].courseName = "Computer Programming";
-      course[4].courseCode = "CC103";
+      course[4].courseName = "Science Technology and Society";
+      course[4].courseCode = "GEC7";
+      course[4].units = 3;
       
-      course[5].courseName = "Computer Programming";
-      course[5].courseCode = "CC103";
+      course[5].courseName = "Reading Visual Art";
+      course[5].courseCode = "GEC8";
+      course[5].units = 2;
       
-      course[6].courseName = "Computer Programming";
-      course[6].courseCode = "CC103";
+      course[6].courseName = "Self Defense";
+      course[6].courseCode = "PE2";
+      course[6].units = 3;
+
+      course[7].courseName = "National Service Training Program 2";
+      course[7].courseCode = "NSTP2";
+      course[7].units = 3;
       
     }
 
@@ -73,7 +90,7 @@ class courses {
 
  
 }
-public class coe {
+public class COE {
  
 void header() {
 
@@ -90,38 +107,39 @@ void header() {
         System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
 }
 
- void a() {
+  void schedGenerator() {
 
+    COE head = new COE();
 
-}
-  
- void b() {
-
-
-}
-       
-     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        
-        coe head = new coe();
-        coe blockA = new coe();
-        coe blockB = new coe();
-       
-        
-      
-      if(block == "a" || "A") {
+   if( == "a" || "A") {
         head.header();
-        blockA.a();//for block a COE scheds details
+        
       } if else (block == "b" || "B"){
         head.header();
-        blockB.b();//for block b COE Scheds details
+        
       } else {
 
         System.out.println("Put valid Block nigga!");
 
       }
+     
 
+}
+  
+
+       
+     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        COE scheds = new COE();
+
+        scheds.schedGenerator();
+       
+        
+
+       
        
       
     }
