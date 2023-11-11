8f409994030c15270a86a2798de9bf1c710c2370
M4cuuuu <tinduganmarkjayt@gmail.com>
2023-11-11 10:12:52 +0800

    fixed alignment

--- COE.java
+++ COE.java
@@ -14,27 +14,34 @@ public static void main(String[] args) {
         student student1 = new student();
 
 
-        System.out.println("Please input the student name: ");
-        student1.full_name = scanner.nextLine();
+        // System.out.println("Please input the student name: ");
+        // student1.full_name = scanner.nextLine();
+        // System.out.println("Please input the student age: ");
+        // student1.age = scanner.nextInt();
+        // scanner.nextLine();
+        // System.out.println("Please input the student block: ");
+        // student1.block = scanner.nextLine();
+        // System.out.println("Please input the student ID: ");
+        // student1.student_id = scanner.nextLine();
         
        
         String school = "Catanduanes State University";
         String school_address = "Virac, Catanduanes";
         String coe = "CERTIFICATE OF ENROLMENT";
         String sy = "2019-2020";
-        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-        System.out.println("\t\t\t\t\t\t\t\t\t\t" + school);
-        System.out.println("\t\t\t\t\t\t\t\t\t\t      " + school_address );
-        System.out.println("\t\t\t\t\t\t\t\t\t\t  " + coe);
-        System.out.println("\t\t\t\t\t\t\t\t\t\t\t " + sy);
-        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("\t\t\t\t\t\t\t" + school);
+        System.out.println("\t\t\t\t\t\t\t      " + school_address );
+        System.out.println("\t\t\t\t\t\t\t  " + coe);
+        System.out.println("\t\t\t\t\t\t\t\t " + sy);
+        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------");
 
-        System.out.printf("Student Name: %s \t\t\t\t Program: %s\n",student1.full_name,student1.program);
-        System.out.printf("Age: %d \n",student1.age);
-        System.out.printf("Block: %s\n",student1.block);
+        System.out.printf("Student Name: %s \t\t\t\t\t\t Program: %s\n",student1.full_name,student1.program);
+        System.out.printf("Age: %d \t\t\t\t\t\t\t\t Student ID: %s \n",student1.age,student1.student_id);
+        System.out.printf("Block: %s \t\t\t\t\t\t\t College: %s\n",student1.block, student1.college);
     
 
-        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------");
         
 
                 course[0] = new Course("Computer Programming 2", "CC103", 4);
@@ -64,13 +71,15 @@ public static void main(String[] args) {
 
   
 
-     System.out.println("\t Code \t\t\t\tCourseName\t\t\t\t\tUnits");
+     System.out.println("\t Code\t\t\tCourseName\t\t\t      Units");
     
                 // Print course names and codes
 
                 for (int i = 0; i < course.length; ++i) {
-                    System.out.println("\t"+course[i].courseCode+ "\t\t\t"+course[i].courseName);//fix here next!
+                    String formattedOutput = String.format("\t%-8s\t%-40s\t%-4d", course[i].courseCode, course[i].courseName, course[i].units);
+                    System.out.println(formattedOutput);
                 }
+                
 
                 
 
