6abd61237c89ec608373cc2c92b41ed67e0032ef
M4cuuuu <tinduganmarkjayt@gmail.com>
2023-11-22 20:43:57 +0800

    changes

--- COE.java
+++ COE.java
@@ -1,8 +1,40 @@
 import java.util.Scanner;
 
-import package1.Course;
-import package1.student;
- 
+ class student {
+    
+    public String full_name;
+    public int age;
+    public String student_id;
+    public String block;
+    public String program = "BSIS";
+    public String college = "CICT";
+    public String address;
+    public int year;
+    public String birthdate;
+
+}
+
+ class Course {
+     
+     public String courseName;
+     public String courseCode;
+     public double units;
+     public String instructor;
+     public double lab;
+     public double tf;
+
+     public Course(String courseName,String courseCode,double units,double lab,double tf,String instructor) {
+
+        this.courseName =courseName;
+        this.courseCode = courseCode;
+        this.lab = lab;//lab units
+        this.tf = tf;//tf units
+        this.units = lab + tf;//total units
+        this.instructor = instructor;
+
+        
+     }
+ }
 
  public class COE {
 
@@ -14,39 +46,57 @@ import package1.student;
         String coe = "CERTIFICATE OF ENROLMENT";
         String sy = "2019-2020";
 
-        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------");
-        System.out.println("\t\t\t\t\t\t\t" + school);
-        System.out.println("\t\t\t\t\t\t\t      " + school_address );
-        System.out.println("\t\t\t\t\t\t\t  " + coe);
-        System.out.println("\t\t\t\t\t\t\t\t " + sy);
-        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------");
+                System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+        System.out.println("\t\t\t\t\t\t\t\t" + school);
+        System.out.println("\t\t\t\t\t\t\t\t      " + school_address );
+        System.out.println("\t\t\t\t\t\t\t\t  " + coe);
+        System.out.println("\t\t\t\t\t\t\t\t\t " + sy);
+                System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
 
         
     }
     
+ 
 public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
          Course[] course = new Course[8];
         student student1 = new student();
         COE head = new COE();
+
+        //Student Details
+        String name = "Name:";
+        String age = "Age:";
+        String stud_id = "Student ID:";
+        String bl = "Block:";
+        String year = "Year:";
+        String clg = "College:";
+        String prog = "Program:";
+        String add = "Address:";
+
         //COE Details
         String cc = "Course Code";
         String cn = "Course Name";
         String u = "Units";
+        String tf ="TF";
+        String lb = "Lab";
         String s = "Schedule";
         String ins = "Instructor";
+        String sec = "Section";
 
-    
+        
 
-        course[0] = new Course("Computer Programming 2", "CC103", 4, "Dr. Jose Rizal");
-        course[1] = new Course("Fundamentals of Information System", "IS211", 3, "Prof. Stephen Hawkins");
-        course[2] = new Course("Organization and Management Concept", "IS212", 3, "Mr. Carlo Kuku Palad");
-        course[3] = new Course("Art Appreciation", "GEC6", 2, "Mr. Vincent Van Gogh");
-        course[4] = new Course("Science Technology and Society", "GEC7", 3, "Mr. Elon Musk");
-        course[5] = new Course("Reading Visual Art", "GEC8", 2, "Mr. Leonardo Da Vinci");
-        course[6] = new Course("Self Defense", "PE2", 3, "Mr. Chuck Norris");
-        course[7] = new Course("National Service Training Program 2", "NSTP2", 3, "Mr. Rodrigo Roa Du30");
+        // Course array
+        course[0] = new Course("Computer Programming 2", "CC103", 0,2.0,3.0, "Dr. Jose Rizal");
+        course[1] = new Course("Fundamentals of Information System", "IS211", 0,2.0,2.0, "Prof. Stephen Hawkins");
+        course[2] = new Course("Organization and Management Concept", "IS212", 0,0.0,3.0, "Mr. Carlo \"Kuku\" Palad");
+        course[3] = new Course("Art Appreciation", "GEC6", 0, 0.0,3.0, "Mr. Vincent Van Gogh");
+        course[4] = new Course("Science Technology and Society", "GEC7", 3.0,0,3.0, "Mr. Elon Musk");
+        course[5] = new Course("Reading Visual Art", "GEC8", 0,0.0,2.0,"Mr. Leonardo Da Vinci");
+        course[6] = new Course("Self Defense", "PE2", 0,0.0,3.0, "Mr. Chuck Norris");
+        course[7] = new Course("National Service Training Program 2", "NSTP2", 0,0.0,3.0, "Mr. Rodrigo Roa Du30");
 
         String [] timeSchedA = new String[8];
         String [] timeSchedB = new String[8]; // fix this next
@@ -62,7 +112,8 @@ public static void main(String[] args) {
         timeSchedA[6] = "7:30am-9:30am";
         timeSchedA[7] = "7:30am-9:30am";
 
-        //extracts the student's details
+        
+        // extracts the student's details
         // System.out.println("Please input the student name: ");
         // student1.full_name = scanner.nextLine();
         // System.out.println("Please input the student age: ");
@@ -75,26 +126,31 @@ public static void main(String[] args) {
         
        //header
         head.header();
-        System.out.printf("Student Name: %s \t\t\t\t\t\t Program: %s\n",student1.full_name,student1.program);
-        System.out.printf("Age: %d \t\t\t\t\t\t\t\t Student ID: %s \n",student1.age,student1.student_id);
-        System.out.printf("Block: %s \t\t\t\t\t\t\t College: %s\n",student1.block, student1.college);
-        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.printf(" %s\t%-50s %s %s\n",name,student1.full_name,prog , student1.program);
+        System.out.printf(" %s \t%-50d %s %s \n",age,student1.age, stud_id,student1.student_id);
+        System.out.printf(" %s\t%-50s %s %s\n",bl,student1.block,clg, student1.college);
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------");
     // Prints inforamtion units
-    String formattedUnits = String.format("\t%s \t\t%s\t\t\t      %s \t\t%s\t\t    %s",cc,cn,u,s,ins);
-
+    String formattedUnits = String.format("\t%s\t\t%s\t\t       %s  %s  %s \t%s\t\t    %s\t\t%s",cc,cn,u,tf,lb,s,ins,sec);
+    String section = student1.program + student1.block;
     System.out.println(formattedUnits);
     
     
                 // Prints all the schedules and information regrading courses enrolled
                 
                 for (int i = 0; i < course.length; ++i) {
-                    String formattedOutput = String.format("\t%-8s\t%-45s\t%-10d %s%s\t\t%s", course[i].courseCode, course[i].courseName, course[i].units,timeSchedA[i],day[i],course[i].instructor);
+                    double unitsWithDecimal = (double) course[i].units; // Convert units to double
+                    String formattedOutput = String.format("\t%-8s\t%-35s\t%-10.1f\t%s%s\t%s\t%-70s", course[i].courseCode, course[i].courseName, unitsWithDecimal, timeSchedA[i], day[i], course[i].instructor, section);
                     System.out.println(formattedOutput);
                 }
                 
+                
+                
+                
 
                 
-     System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------");
+             System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
 
 
 scanner.close();
